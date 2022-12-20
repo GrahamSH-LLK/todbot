b23cbd53c4723ac1e29d1ef03f732ff5ad8c3cc7
GrahamSH <grahamshllk@gmail.com>
2022-12-20 02:03:52 +0000

    Fun changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Client, Events, GatewayIntentBits, Partials } from "discord.js";
 import config from "./config.js";
-import fs from 'fs'
+import fs from "fs";
 const { token } = config;
 import { importScripts, extractMessageExtremities } from "./util.js";
 import dare from "./commands/dare.js";
@@ -8,26 +8,40 @@ import truth from "./commands/truth.js";
 import random from "./commands/random.js";
 import path from "node:path";
 import * as url from "node:url";
+import http from "http";
+import fetch from "node-fetch";
 const dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
   partials: [Partials.Message],
 });
-console.log()
-
+console.log();
+const getDataURI = async (url) => {
+  const response = await fetch(url);
+  const blob = await response.arrayBuffer();
+  return `data:${response.headers.get("content-type")};base64,${Buffer.from(
+    blob
+  ).toString("base64")}`;
+};
 client.once(Events.ClientReady, (c) => {
   client.on("messageDelete", async (messageDelete) => {
-    return
-    console.log(messageDelete)
-    let {files} = await extractMessageExtremities(messageDelete)
-    let content = `${messageDelete.author.username}: ${messageDelete.content} (${files.reduce((acc, val)=> {
-      return acc + val.url
-    }, '')})\n`;
+    console.log(messageDelete);
+    let { files } = await extractMessageExtremities(messageDelete);
+    console.log(await getDataURI(files[0].url))
+    let content = `${messageDelete.author.username}: ${
+      messageDelete.content
+    } (${await files.reduce(async (acc, val) => {
+      return acc + `<img src="${await getDataURI(val.url)}"> `;
+    }, "")})\n`;
     fs.appendFile("db.txt", content, (err) => {
       return console.log(err);
     });
   });
-  
+
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 const commands = importScripts(path.resolve(dirname, "./commands"));
@@ -54,6 +68,25 @@ client.on(Events.InteractionCreate, (interaction) => {
   if (interaction.customId == "random") random.execute(interaction);
   else if (interaction.customId == "truth") truth.execute(interaction);
   else if (interaction.customId == "dare") dare.execute(interaction);
+});
+const start = `<!DOCTYPE html><html><link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+<head></head><body><ul>`;
+const end = `</body></html>`;
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  fs.readFile("db.txt", function (err, data) {
+    if (err) throw err;
+    var array = data.toString().split("\n");
+    let reduced = array.reduce((previousValue, currentValue) => {
+      return `${previousValue} <li> ${currentValue}</li>`;
+    }, "");
+    res.end(`${start}${reduced}${end}`);
+  });
+};
+const server = http.createServer(requestListener);
+server.listen(8090, "localhost", () => {
+  console.log(`Server is running on http://localhost:8090`);
 });
 
 client.login(token);
